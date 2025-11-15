581a1362aa604c3ac4c0dcfdcad0b8d780cd5ce8
Daniel <danielflored92@gmail.com>
2025-11-15 16:06:55 -0400

    json por socket

--- src/app/app.py
+++ src/app/app.py
@@ -1,7 +1,23 @@
+import json
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
+
+origins = [
+    '*',
+    ### "http://localhost",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = origins,
+    allow_credentials=True,
+    allow_methods=['*'],  # Permite todos los métodos
+    allow_headers=['*'],
+)
+
 
 
 @app.get('/', )
@@ -15,8 +31,25 @@ async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
     try:
         while True:
-            pass
-    
+            raw_message = await websocket.receive_text()
+            
+            if not raw_message.strip():
+                print('empty string')
+                continue
+            
+            if '\n' in raw_message:
+                print('logica a')
+            
+            else:
+            
+                print(raw_message)
+                
+        
+            
+            
+           
     
     except WebSocketDisconnect:
         print(f'cliente desconectado')
+    finally:
+        await websocket.close()
